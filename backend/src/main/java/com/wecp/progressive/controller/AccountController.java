27bc7f6082ef552761a3f8635653ddde49dff03e
username <wecp-dummy@gmail.com>
2024-02-01 12:06:24 +0000

    day8 passed

--- backend/src/main/java/com/wecp/progressive/controller/AccountController.java
+++ backend/src/main/java/com/wecp/progressive/controller/AccountController.java
@@ -2,7 +2,6 @@ package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Accounts;
 import com.wecp.progressive.service.AccountService;
-import com.wecp.progressive.service.CustomerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.SQLException;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
@@ -19,37 +19,29 @@ public class AccountController {
     @Autowired
     AccountService accountService;
 
+    @GetMapping
     public ResponseEntity<List<Accounts>> getAllAccounts() throws SQLException {
-        return new ResponseEntity<>(accountService.getAllAccounts(), HttpStatus.OK);
+        return new ResponseEntity<List<Accounts>>(accountService.getAllAccounts(), HttpStatus.OK);
     }
 
     @GetMapping("/{accountId}")
     public ResponseEntity<Accounts> getAccountById(@PathVariable int accountId) throws SQLException {
         return new ResponseEntity<Accounts>(accountService.getAccountById(accountId), HttpStatus.OK);
     }
-
-    public ResponseEntity<List<Accounts>> getAccountsByUser(String param) throws SQLException {
-        // return new
-        return new  ResponseEntity<>(accountService.getAccountsByUser(Integer.parseInt(param)),HttpStatus.OK);
-        
+    @GetMapping("/user/{param}")
+    public ResponseEntity<List<Accounts>> getAccountsByUser(@PathVariable String param) throws NumberFormatException, SQLException {
+        return new ResponseEntity<List<Accounts>>(accountService.getAccountsByUser(Integer.parseInt(param)), HttpStatus.OK);
     }
-
     @PostMapping
     public ResponseEntity<Integer> addAccount(@RequestBody Accounts accounts) throws SQLException {
-        return new ResponseEntity<>(accountService.addAccount(accounts), HttpStatus.CREATED);
+        return new ResponseEntity<Integer>(accountService.addAccount(accounts), HttpStatus.CREATED);
+    }
+    
+    public ResponseEntity<Void> updateAccount(int accountId, Accounts accounts) {
+        return null;
     }
 
-    @PutMapping("/{accountId}")
-    public ResponseEntity<Void> updateAccount(@PathVariable int accountId, @RequestBody Accounts accounts)
-            throws SQLException {
-        accounts.setAccountId(accountId);
-        accountService.updateAccount(accounts);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{accountId}")
-    public ResponseEntity<Void> deleteAccount(int accountId) throws SQLException {
-        accountService.deleteAccount(accountId);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<Void> deleteAccount(int accountId) {
+        return null;
     }
 }
